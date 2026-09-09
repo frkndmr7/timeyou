529532d73a61c1e95d2bf4258e51b80e34d2293e
Halil Furkan Damar <furkan@Halil-MacBook-Air.local>
2026-09-10 01:32:32 +0300

    Add timeyou.co ALIAS record

--- infrastructure/terraform/route53.tf
+++ infrastructure/terraform/route53.tf
@@ -1,3 +1,21 @@
 resource "aws_route53_zone" "timeyou" {
   name = "timeyou.co"
 }
+
+data "aws_lb" "timeyou_dev" {
+  tags = {
+    "ingress.k8s.aws/stack" = "timeyou-dev/timeyou"
+  }
+}
+
+resource "aws_route53_record" "timeyou_apex" {
+  zone_id = aws_route53_zone.timeyou.zone_id
+  name    = "timeyou.co"
+  type    = "A"
+
+  alias {
+    name                   = data.aws_lb.timeyou_dev.dns_name
+    zone_id                = data.aws_lb.timeyou_dev.zone_id
+    evaluate_target_health = true
+  }
+}
